8a09c9b5dcd08e1eb8267900b8bba1cee9479966
MuratArs1an <kilicarslan87@gmail.com>
2023-11-19 23:27:45 +0300

    swagger config added

---
+++ src/main/java/com/muratarslan/stockmanagement/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muratarslan.stockmanagement.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
